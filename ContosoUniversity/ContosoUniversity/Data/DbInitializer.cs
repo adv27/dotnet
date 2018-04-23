ec3e3c815beeb42f7bb8d331b3c562e9fc996445
Dinh Anh Vu <anhvdse04605@fpt.edu.vn>
2018-04-23 11:08:21 +0700

    first commit

---
+++ ContosoUniversity/ContosoUniversity/Data/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize (SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Students.Any())
+            {
+                return;
+            }
+        }
+    }
+}
