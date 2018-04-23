4408ad3866533dad8a1c762260eda744d286d7a2
Dinh Anh Vu <anhvdse04605@fpt.edu.vn>
2018-04-23 14:45:48 +0700

    step 2/10

--- ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -7,11 +7,11 @@ namespace ContosoUniversity.Models
 {
     public enum Grade
     {
-         A, B, C, D, E
+         A, B, C, D, E, F
     }
     public class Enrollment
     {
-        public int ErollmentID { get; set; }
+        public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
         public Grade? Grade { get; set; }
